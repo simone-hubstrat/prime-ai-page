82b9b8fc851aba800b8f9ff2eeb8113007b22423
Kaitao0 <65125712+Kaitao0@users.noreply.github.com>
2025-09-17 11:34:54 +0200

    Updated Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import HeroKiller from '../components/sections/HeroKiller';
 import SocialProof from '../components/sections/SocialProof';
+import StrategicPricing from '../components/sections/StrategicPricing';
+import TargetSection from '../components/sections/TargetSection';
 import FinalKiller from '../components/sections/FinalKiller';
+import SimpleFooter from '../components/sections/SimpleFooter';
 
 const Index: React.FC = () => {
   return (
-    <main className="w-full min-h-screen">
+    <main className="w-full min-h-screen flex flex-col">
       <HeroKiller />
       <SocialProof />
+      <StrategicPricing />
+      <TargetSection />
       <FinalKiller />
+      <SimpleFooter />
     </main>
   );
 };
